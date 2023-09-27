087aa70b5ed37e2c2f40abd142f5dd16d3ec05b7
Phan Đức Nghĩa <123baconsau6969@gmail.com>
2023-09-27 08:37:13 +0700

    làm lẹ

--- CircleLinkedList.py
+++ CircleLinkedList.py
@@ -3,13 +3,62 @@ class CircleLinkedList:
         self.head = None
         self.size = 0
 
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
     def isEmpty(self):
         return self.size == 0
 
-    def insertFirst(self, e):
-        self.head = Node(e, self.head)
-        self.size += 1
+    def insertfirst(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
+            new_node.next = self.head
+            self.head = new_node
 
-    def insertLast(self, e):
-        if self.isEmpty():
-            self.head = Node(e, None)
+    def insertlast(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            temp = self.head
+            while temp.next != self.head:
+                temp = temp.next
+            temp.next = new_node
+            new_node.next = self.head
+
+    def insertafter(self, afterdata, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            self.head.next = self.head
+        else:
+            temp = self.head
+            while temp is not None:
+                if temp.data == afterdata:
+                    break
+                temp = temp.next
+            if temp is None:
+                print("Phần tử không tồn tại trong danh sách")
+                return
+            new_node.next = temp.next
+            temp.next = new_node
+
+    def display(self):
+        nodes = []
+        temp = self.head
+        while True:
+            nodes.append(temp.data)
+            temp = temp.next
+            if temp == self.head:
+                break
+        print(" -> ".join(map(str, nodes)))
