6caac3cfcd8ba3b7cf72658199217658049d8b9a
thienphuprogrammer <thienphuprogrammer@gmail.com>
2023-09-27 08:16:45 +0700

    1

--- StudentManager/main.py
+++
@@ -1,9 +0,0 @@
-from Menu import Menu
-
-if __name__ == '__main__':
-    menu = Menu()
-    while True:
-        menu.showMenu()
-        choice = menu.getChoiceFromUser()
-        if not menu.handleUserChoice(choice):
-            break
