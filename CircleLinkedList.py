26610ddeba33052c7a1e5b949d3012e1aec53aec
thienphuprogrammer <thienphuprogrammer@gmail.com>
2023-09-27 15:17:33 +0700

    h

--- CircleLinkedList.py
+++ CircleLinkedList.py
@@ -1,65 +1,154 @@
-
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self, element, next=None):
+        self.element = element
+        self.next = next
+
 
 class CircleLinkedList:
     def __init__(self):
-        self.head = None
+        self.last_node = None
         self.size = 0
 
     def isEmpty(self):
-        return self.size == 0
+        return self.last_node is None
 
-    def insertfirst(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.head.next = self.head
-        else:
-            temp = self.head
-            while temp.next != self.head:
-                temp = temp.next
-            temp.next = new_node
-            new_node.next = self.head
-            self.head = new_node
+    def size(self):
+        return self.size
 
-    def insertlast(self, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.head.next = self.head
-        else:
-            temp = self.head
-            while temp.next != self.head:
-                temp = temp.next
-            temp.next = new_node
-            new_node.next = self.head
+    def get(self, index):
+        if self.isEmpty():
+            return None
 
-    def insertafter(self, afterdata, data):
-        new_node = Node(data)
-        if not self.head:
-            self.head = new_node
-            self.head.next = self.head
+        current = self.last_node.next
+        for i in range(index):
+            current = current.next
+        return current.element
+
+    def indexOf(self, item):
+        current = self.last_node.next
+        index = 0
+        while current is not None:
+            if current.element == item:
+                return index
+            index += 1
+            current = current.next
+        return -1
+
+    def remove(self, item):
+        current = self.last_node.next
+        previous = None
+        found = False
+        while current is not None and not found:
+            if current.element == item:
+                found = True
+            else:
+                previous = current
+                current = current.next
+        if previous is None:
+            self.last_node = current
         else:
-            temp = self.head
-            while temp is not None:
-                if temp.data == afterdata:
-                    break
-                temp = temp.next
-            if temp is None:
-                print("Phần tử không tồn tại trong danh sách")
-                return
-            new_node.next = temp.next
-            temp.next = new_node
+            previous.next = current.next
+        self.size -= 1
+        return found
+
+    def deleteAfter(self, value):
+        current = self.last_node.next
+        previous = None
+        found = False
+        while current is not None and not found:
+            if current.element == value:
+                found = True
+            else:
+                previous = current
+                current = current.next
+        if previous is None:
+            self.last_node = current
+        else:
+            previous.next = current.next
+        self.size -= 1
+        return
+
+    def deleteFirst(self):
+        if self.last_node is None:
+            return
+        self.last_node = self.last_node.next.next
+        self.size -= 1
+        return
+
+    def deleteLast(self):
+        if self.last_node is None:
+            return
+        current = self.last_node.next
+        while current.next is not self.last_node:
+            current = current.next
+        current.next = self.last_node.next
+        self.last_node = current
+        self.size -= 1
+        return
+
+    def deleteAtIndex(self, index):
+        if index < 0:
+            print("Invalid index")
+            return
+        if index >= self.size:
+            self.deleteLast()
+        current = self.last_node.next
+        previous = None
+        for i in range(index):
+            previous = current
+            current = current.next
+        previous.next = current.next
+        self.size -= 1
+        return
+
+    def insertFirst(self, item):
+        temp = Node(item)
+        if self.last_node is None:
+            self.last_node = temp
+            self.last_node.next = self.last_node
+        else:
+            temp.next = self.last_node.next
+            self.last_node.next = temp
+        self.size += 1
+        return
+
+    def insertLast(self, item):
+        temp = Node(item)
+        if self.last_node is None:
+            self.last_node = temp
+            self.last_node.next = self.last_node
+        else:
+            temp.next = self.last_node.next
+            self.last_node.next = temp
+            self.last_node = temp
+        self.size += 1
+        return
+
+    def insertAtIndex(self, index, item):
+        if index < 0:
+            print("Invalid index")
+            return
+        if index >= self.size:
+            self.insertLast(item)
+        temp = Node(item)
+        current = self.last_node.next
+        previous = None
+        for i in range(index):
+            previous = current
+            current = current.next
+        previous.next = temp
+        temp.next = current
+        self.size += 1
+        return
 
     def display(self):
-        nodes = []
-        temp = self.head
+        current = self.last_node.next
         while True:
-            nodes.append(temp.data)
-            temp = temp.next
-            if temp == self.head:
+            print(current.element, end=" ")
+            current = current.next
+            if current != self.last_node.next:
+                print("->", end=" ")
+            else:
                 break
-        print(" -> ".join(map(str, nodes)))
+        print()
+        return
--- Menu.py
+++ Menu.py
@@ -1,41 +1,113 @@
+from enum import Enum
 from CircleLinkedList import CircleLinkedList
 
-def Menu():
-    print("+----------------------+")
-    print("| 1.Insert first       |")
-    print("| 2.Insert last        |")
-    print("| 3.Insert after       |")
-    print("| 4.display            |")
-    print("| 5.delete first       |")
-    print("| 6.delete last        |")
-    print("| 7.delete after       |")
-    print("| 8.get size           |")
-    print("+----------------------+")
 
-    while True:
-        choice = int(input('Nhập lựa chọn của ní:'))
+class MenuChoice(Enum):
+    EXIT = 0
+    INSERT_FIRST = 1
+    INSERT_LAST = 2
+    INSERT_AFTER = 3
+    DELETE_FIRST = 4
+    DELETE_LAST = 5
+    DELETE_AFTER = 6
+    UPDATE = 7
+    SHOW_INFORMATION = 8
+    SEARCH = 9
 
-        if choice == 1:
-            data = input()
-            CircleLinkedList.insertfirst(data)
-        elif choice == 2:
-            data = input()
-            CircleLinkedList.insertlast(data)
-        elif choice == 3:
-            data = input()
-            CircleLinkedList.insertafter(data)
-        elif choice == 4:
-            data = input()
-            CircleLinkedList.display()
-        elif choice == 5:
-            print('coming soon')
-        elif choice == 6:
-            print('coming soon')
-        elif choice == 7:
-            print('coming soon')
-        elif choice == 8:
-            print('coming soon')
-        else:
-            print('lựa chọn chỉ trong khoản từ 1 - 8')
 
-Menu()
+class Menu:
+    def __init__(self):
+        self.list = CircleLinkedList()
+
+    @staticmethod
+    def showMenu():
+        try:
+            print("-" * 20 + " MENU " + "-" * 20)
+            for choice in MenuChoice:
+                print(f"| Choice {choice.value} to {' '.join(choice.name.split('_')).lower()}." + " " * (
+                        30 - len(choice.name)) + "|")
+            print("-" * 46)
+            choice = Menu.getChoiceFromUser()
+            Menu.handleUserChoice(choice)
+        except ValueError:
+            print("Invalid choice. Please enter a valid choice.")
+
+    @staticmethod
+    def getChoiceFromUser():
+        while True:
+            try:
+                choice = int(input('Enter your choice (0 to exit): '))
+                return choice
+            except ValueError:
+                print("Invalid choice. Please enter a valid choice.")
+
+    def handleUserChoice(self, choice):
+        if choice == MenuChoice.EXIT:
+            print("Exitted program!!!!")
+            status = False
+        elif choice == MenuChoice.INSERT_FIRST:
+            print("Insertting....")
+            self.handleInsertFirst()
+            pass
+        elif choice == MenuChoice.INSERT_LAST:
+            print("Insertting....")
+            self.handleInsertLast()
+            pass
+        elif choice == MenuChoice.INSERT_AFTER:
+            print("Insertting....")
+            self.handleInsertAfter()
+            pass
+        elif choice == MenuChoice.DELETE_FIRST:
+            print("Insertting....")
+            self.handleDeleteFirst()
+            pass
+        elif choice == MenuChoice.DELETE_LAST:
+            print("Deleting...")
+            self.handleDeleteLast()
+            pass
+        elif choice == MenuChoice.DELETE_AFTER:
+            print("Deleting...")
+            self.handleDeleteAfter()
+            pass
+        elif choice == MenuChoice.SHOW_INFORMATION:
+            print("Show...")
+            self.handleShow()
+            pass
+
+    def handleInsertFirst(self):
+        print("Input value: ", end='')
+        value = int(input())
+        self.list.insertFirst(value)
+        pass
+
+    def handleInsertLast(self):
+        print("Input value: ", end='')
+        value = int(input())
+        self.list.insertLast(value)
+        pass
+
+    def handleInsertAfter(self):
+        print("Input value: ", end='')
+        value = int(input())
+        print("Input index: ", end='')
+        index = int(input())
+        self.list.insertAtIndex(index, value)
+        pass
+
+    def handleDeleteFirst(self):
+        self.list.deleteFirst()
+        pass
+
+    def handleDeleteLast(self):
+        self.list.deleteLast()
+        pass
+
+    def handleDeleteAfter(self):
+        print("Input index: ", end='')
+        index = int(input())
+        self.list.deleteAtIndex(index)
+        pass
+
+    def handleShow(self):
+        self.list.display()
+        pass
